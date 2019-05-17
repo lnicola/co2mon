541c22a21eee776e7414a598279dd09b49d8242c
Laurențiu Nicola <lnicola@dend.ro>
2019-05-17 15:08:57 +0300

    Update doc URL

--- co2mon/src/lib.rs
+++ co2mon/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/co2mon/1.0.0-rc.3")]
+#![doc(html_root_url = "https://docs.rs/co2mon/1.0.0")]
 #![deny(missing_docs)]
 
 //! A driver for the Holtek ([ZyAura ZG][ZG]) CO₂ USB monitors.
